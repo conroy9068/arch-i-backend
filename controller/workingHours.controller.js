99748eb425ddf61637d5ea86699a627a6d1da719
rkshawon <rkshawon16@gmail.com>
2022-10-06 10:10:01 -0700

    updated

--- controller/workingHours.controller.js
+++ controller/workingHours.controller.js
@@ -217,10 +217,10 @@ const organizeData = (data, edata) => {
     const pdata = {
       project_name: d.dataValues.project.dataValues.project_name,
       start_time: d.dataValues.start_time,
-      end_time: "d.dataValues.end_time",
     };
-
-    employee_data.project.push(pdata);
+    if (d.dataValues.end_time === null || !d.dataValues.end_time) {
+      employee_data.project.push(pdata);
+    }
   });
   if (employee_data.project.length === 0) {
     employee_data.project = null;
