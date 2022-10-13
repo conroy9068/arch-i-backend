c07756c7d83ccf6e97ea42f2c25973e3bcb9e4aa
rkshawon <rkshawon16@gmail.com>
2022-10-13 13:00:41 -0700

    updated

--- database/models/employee.js
+++ database/models/employee.js
@@ -1,13 +1,19 @@
 module.exports = (sequelize, Sequelize) => {
-  const Employee = sequelize.define("employee", {
-    employee_id: {
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
-      primaryKey: true,
+  const Employee = sequelize.define(
+    "employee",
+    {
+      employee_id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(50),
+      },
     },
-    name: {
-      type: Sequelize.STRING(50),
-    },
-  });
+    {
+      timestamps: false,
+    }
+  );
   return Employee;
 };
